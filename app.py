019f2c4504f04416879ecf875bd37344309b2747
gdotmount <59402975+gdotmount@users.noreply.github.com>
2020-07-13 14:49:19 -0500

    screwing around

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import discord
-client = discord.Client()
+client=discord.Client
+class MyClient(client):
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith('hello'):
+            await message.channel.send('hello there')
+
+from discord.ext import commands
+bot = commands.Bot(command_prefix='#')
+@bot.command
+async def beep(ctx):
+    await ctx.channel.send('boop')
+
 client.run('NjkyNDUzMzkxMDkxMzY4MDE3.XwwdKg.U_r_huoQ-FpZ-POZ9C9MGRZnqoA')
