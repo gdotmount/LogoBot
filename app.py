3fea9afbd478c9acc351e20a5aa17ac91fd78ca7
gdotmount <59402975+gdotmount@users.noreply.github.com>
2020-07-13 03:37:29 -0500

    new token

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import discord
 client = discord.Client()
-client.run('NjkyNDUzMzkxMDkxMzY4MDE3.XwwL4Q.Xo0ThereVdzhQxljoUpOi9SPOcg_b4')
+client.run('NjkyNDUzMzkxMDkxMzY4MDE3.XwwdKg.U_r_huoQ-FpZ-POZ9C9MGRZnqoA')
