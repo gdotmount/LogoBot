9c7a542d3878484e9fe86becc569d77d876562b3
Gabe Mount <gmaxmount@gmail.com>
2020-08-10 13:16:11 -0500

    Working with Postgres tables

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-import json
 import os
+import random
 import psycopg2
 import discord
 from discord.ext import commands
+from discord.utils import get
 
 connection = psycopg2.connect(
     user = os.environ['DATABASE_USER'], 
@@ -28,33 +29,77 @@ async def on_message(message):
 
 @bot.event
 async def on_guild_join(guild):
-    return
+    sql = "CREATE TABLE IF NOT EXISTS s%d (domain varchar(100), userid BIGINT PRIMARY KEY, code INT, tagline varchar(100), verified BOOLEAN);" % guild.id
+    cursor.execute(sql)
+    sql = "INSERT INTO s%d (domain, userid) VALUES('@gmail.com', 0);" & guild.id
+    cursor.execute(sql)
+    for member in guild.members:
+        sql = 'INSERT INTO s%d (userid, verified) VALUES(%d, TRUE);' % (guild.id, member.id)
+        cursor.execute(sql)
+    connection.commit()
 
 @bot.command()
-async def confirm(ctx):
-    json_str = "'{ " + f'"{ctx.guild.id}":' + ' { "domain": "@gmail.com", "users": [] } }' + "'"
-    print(json_str)
-    await ctx.send('boop')
-    function = f"""INSERT INTO servers (info)
-        VALUES({json_str})"""
-    cursor.execute(function)
+async def initdb(ctx):
+    sql = "CREATE TABLE IF NOT EXISTS s%d (domain varchar(100), userid BIGINT PRIMARY KEY, code INT, tagline varchar(100), verified BOOLEAN);" % ctx.guild.id
+    cursor.execute(sql)
+    sql = "INSERT INTO s%d (domain, userid) VALUES('@gmail.com', 0);" % ctx.guild.id
+    cursor.execute(sql)
+    for member in ctx.guild.members:
+        sql = 'INSERT INTO s%d (userid, verified) VALUES(%d, TRUE);' % (ctx.guild.id, member.id)
+        cursor.execute(sql)
     connection.commit()
-    
+
 @bot.command()
-async def con(ctx):
-    function = "SELECT info FROM servers;"
-    cursor.execute(function)
+async def confirm(ctx, tag):
+    sql = 'select domain from s%d where userid = 0' % ctx.guild.id
+    cursor.execute(sql)
+    domain = cursor.fetchall()[0][0]
+    sql = 'SELECT * FROM s%d' % (ctx.guild.id)
+    cursor.execute(sql)
     results = cursor.fetchall()
-    print(results)
+    exists = False
+    for row in results:
+        if(ctx.author.id == row[1]):
+            if(tag == str(row[2]) and not row[4]):
+                try:
+                    role = get(ctx.guild.roles, name="Verified")
+                    await ctx.author.add_roles(role)
+                except:
+                    await ctx.send('Your code was correct, but Logobot encountered an error.')
+                    message = '''A server admin must specify a verified role to be given upon verification since the default role that Logobot creates upon joining does not exist.'''
+                    await ctx.send(message)
+            elif(not row[4]):
+                await ctx.send(f"Resending verification code to {tag}{domain}")
+                if(tag != row[2]):
+                    sql = "update s%d set tagline = '%s' where userid = %d" % (ctx.guild.id, tag, ctx.author.id)
+                    cursor.execute(sql)
+                    connection.commit()
+            elif(row[4]):
+                await ctx.send('You are already verified dummy.')
+                await ctx.send('jesus i sound like dr berger')
+            exists = True
+            break
+    if(exists == False):
+        code = random.randint(100000, 999999)
+        print(code)
+        sql = "INSERT INTO s%d(userid, code, tagline, verified) VALUES(%d, %d, '%s', FALSE);" % (ctx.guild.id, ctx.author.id, code, tag)
+        cursor.execute(sql)
+        connection.commit()
+        await ctx.send(f'Sending verification code to {tag}{domain}')
     
 @bot.command()
 async def setdomain(ctx, domain):
-    rel_dir = f'server-specific-data/{ctx.guild.id}.json'
-    with open(os.path.join(script_dir, rel_dir), 'r+') as f:
-        data = json.load(f)
-        data['domain'] = domain
-        f.seek(0)
-        json.dump(data, f)
-        f.truncate()
+    sql = "UPDATE s%d SET domain = '%s' where userid = 0;" % (ctx.guild.id, domain)
+    cursor.execute(sql)
+    connection.commit()
+
+    sql = 'SELECT domain FROM s%d where userid = 0' % ctx.guild.id
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(f'New domain for server "{ctx.guild.name}": ' + str(results))
+   
+@bot.command()
+async def test(ctx):
+    return
 
 bot.run(os.environ['BOT_TOKEN'])
